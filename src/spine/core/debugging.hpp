62708304cd2756bb933303ebe2e8d93589108b9b
Stan Verschuuren <31728406+s-t-a-n@users.noreply.github.com>
2024-10-09 16:17:46 +0200

    core/debugging: use spn namespaced macros
    - Changes assert to spn_assert, expect to spn_expect
    - Changes LOG to SPN_LOG, etc.
    - Adds LOG as alias to SPN_LOG (and others respectively) if they are not
      yet defined.
    
    Library refactor to reflect these changes in a separate commit.

--- src/spine/core/debugging.hpp
+++ src/spine/core/debugging.hpp
@@ -2,24 +2,16 @@
 
 #include "spine/core/exception.hpp"
 
-#include <cstdio>
-
-#if defined(DEBUG_SPINE)
-
-#    include "spine/core/logging.hpp"
-
+#ifdef spn_assert
+#    error "spn_assert is already defined"
 #endif
 
-#ifdef assert
-#    undef assert
+#ifdef spn_expect
+#    error "spn_expect is already defined"
 #endif
 
-#ifdef expect
-#    undef expect
-#endif
-
-#ifdef DBG
-#    error "DBG is already defined"
+#ifdef SPN_DBG
+#    error "SPN_DBG is already defined"
 #endif
 
 #if defined(DEBUG_SPINE)
@@ -27,7 +19,7 @@
 #    include "spine/core/logging.hpp"
 
 /// Macro to run assertions which, if failing, will log the failed assertion and throw a spn::exception
-#    define assert(__SPINEDEBUG_condition)                                                                             \
+#    define spn_assert(__SPINEDEBUG_condition)                                                                         \
         {                                                                                                              \
             if (!(__SPINEDEBUG_condition)) {                                                                           \
                 ERR("Assertion failed: '%s'", #__SPINEDEBUG_condition);                                                \
@@ -36,23 +28,28 @@
         }
 
 /// Macro to run expectations which, if failing, will log the failed expectation
-#    define expect(__SPINEDEBUG_condition)                                                                             \
+#    define spn_expect(__SPINEDEBUG_condition)                                                                         \
         {                                                                                                              \
             if (!(__SPINEDEBUG_condition)) {                                                                           \
                 ERR("Expectation failed: '%s'", #__SPINEDEBUG_condition);                                              \
             }                                                                                                          \
         }
 
-/// Macro to print debugging statements, output is prefixed with '!'
-#    define DBG(...) LOG_IMPL(spn::logging::LogLevel::DEBUG, __VA_ARGS__)
+/// Macro to print debugging statements. Only is compiled in when SPINE_DEBUG is defined.
+#    define SPN_DBG(...) SPN_LOG_IMPL(spn::logging::LogLevel::DEBUG, __VA_ARGS__)
 #else
-#    define assert(condition)                                                                                          \
+#    define spn_assert(condition)                                                                                      \
         do {                                                                                                           \
         } while (0);
-#    define expect(condition)                                                                                          \
+#    define spn_expect(condition)                                                                                      \
         do {                                                                                                           \
         } while (0);
-#    define DBG(...)                                                                                                   \
+#    define SPN_DBG(...)                                                                                               \
         do {                                                                                                           \
         } while (0);
+#endif
+
+// Alias for SPN_DBG if it was not previously defined.
+#ifndef DBG
+#    define DBG(...) SPN_DBG(__VA_ARGS__)
 #endif
