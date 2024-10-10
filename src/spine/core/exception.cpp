446a1b0fc4594fe5a9c866db5419e58da711709f
Stan Verschuuren <31728406+s-t-a-n@users.noreply.github.com>
2024-10-11 00:35:11 +0200

    core/exception: use static instead of global exception handler
    - Using a global std::unique_ptr forces to define at_exit() handlers for
      non-trivial global data. By moving the exception handler into an
      access function, we can instead just have a static handle.

--- src/spine/core/exception.cpp
+++ src/spine/core/exception.cpp
@@ -9,15 +9,18 @@
 namespace spn::core {
 
 namespace {
-std::unique_ptr<ExceptionHandler> _g_handler = nullptr;
+std::unique_ptr<ExceptionHandler>& static_eh() {
+    static std::unique_ptr<ExceptionHandler> handler = nullptr;
+    return handler;
 }
+} // namespace
 
 void set_machine_exception_handler(std::unique_ptr<ExceptionHandler> handler) {
-    spn_assert(_g_handler == nullptr);
-    _g_handler.swap(handler);
+    if (static_eh() != nullptr) SPN_DBG("Replacing machine wide exception handler!");
+    static_eh().swap(handler);
 }
 
-ExceptionHandler* machine_exception_handler() { return _g_handler.get(); }
+ExceptionHandler* machine_exception_handler() { return static_eh().get(); }
 
 void Exception::throw_error() const {
     if (const auto eh = machine_exception_handler(); eh != nullptr) {
