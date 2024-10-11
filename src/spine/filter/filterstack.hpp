f2212b41cc0ecb02f321b84ba749a283925b8648
Stan Verschuuren <31728406+s-t-a-n@users.noreply.github.com>
2024-10-12 01:07:19 +0200

    filter: add helper function to filterstack

--- src/spine/filter/filterstack.hpp
+++ src/spine/filter/filterstack.hpp
@@ -17,14 +17,14 @@ public:
 
     /// Attach a filter to the provided slot's index
     void attach_filter(std::unique_ptr<Filter<ValueType>> filter) {
-        spn_assert(_filters.size() < _filters.max_size());
-        _filters.emplace_back(std::move(filter));
+        spn_expect(_filters.size() < _filters.max_size());
+        if (_filters.size() < _filters.max_size()) _filters.emplace_back(std::move(filter));
     }
 
     /// Detach a filter for a provided slot's index
     std::unique_ptr<Filter<ValueType>> detach_filter(uint8_t index) {
-        spn_assert(index < _filters.size());
-        return _filters.remove(index);
+        spn_expect(index < _filters.size());
+        return index < _filters.size() ? _filters.remove(index) : nullptr;
     }
 
     /// Take a new sample, returns filtered value
@@ -32,7 +32,7 @@ public:
         ValueType new_value = new_sample;
         for (auto& sf : _filters) {
             spn_assert(sf.get() != nullptr);
-            new_value = sf->value(new_value);
+            if (sf.get() != nullptr) new_value = sf->value(new_value);
         }
         return new_value;
     }
@@ -50,8 +50,11 @@ public:
         }
     }
 
-    /// Returns pre-allocated slots for filters
+    /// Returns count of pre-allocated filter slots
     size_t filter_slots() const { return _filters.max_size(); }
+
+    /// Returns count of occupied filter slots
+    size_t filter_slots_occupied() const { return _filters.size(); }
 
 private:
     /// todo: cannot use spn/structure/vector; : inplace allocator is missing, or better even to use a linked list
