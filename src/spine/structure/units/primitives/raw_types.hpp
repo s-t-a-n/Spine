f0a4fb8d4fbbd6ff6f0d85e896b3f03557e63950
Stan Verschuuren <31728406+s-t-a-n@users.noreply.github.com>
2024-10-22 17:15:42 +0200

    structure/units: buildflag to optionally use doubles
    - Adds non-default buildflag `SPN_PLATFORM_CAP_DOUBLE` that tells Spine to
      use doubles where applicable. Only used in structure/units at the
      moment.

--- src/spine/structure/units/primitives/raw_types.hpp
+++ src/spine/structure/units/primitives/raw_types.hpp
@@ -4,9 +4,15 @@
 
 namespace spn::structure::units {
 
-using RealRawType = float;
 using IntegerRawType = long long;
 using KernelTimeRawType = time_t;
+
+#ifdef SPN_PLATFORM_CAP_DOUBLE
+using TimeRawType = double;
+using RealRawType = double;
+#else
 using TimeRawType = float;
+using RealRawType = float;
+#endif
 
 } // namespace spn::structure::units
